5a3224434ee063b7df2d0b49f1764ce7f197c435
Tref <nick@treflabs.com>
2015-10-30 14:22:05 -0400

    added model validations  and callbak structure

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,13 @@
 class Link < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :full_url, :short_url, presence: true, uniqueness: true
+  validates :full_url, url: true
+  before_validation :shrink
+
+  private
+  
+    def shrink
+      
+    end
+
 end
