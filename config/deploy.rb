6d6e5936daacec54b54e9a4708c9d1d29c98896b
Tref <nick@treflabs.com>
2015-11-03 22:44:15 -0500

    adding app yaml to git ignore and removing bin from linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :linked_files, %w{config/database.yml config/application.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 
 # Default value for default_env is {}
